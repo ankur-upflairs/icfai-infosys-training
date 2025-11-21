94cc289938c89fb37904e142531e8f317e630d89
ankur-upflairs <ankur.upflairs@gmail.com>
2025-11-21 15:22:28 +0530

    code

--- flask/basics/app.py
+++ flask/basics/app.py
@@ -1,20 +1,15 @@
 from flask import Flask,render_template 
-from products import routes
-from users.routes import user
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
-db=SQLAlchemy(app)
+db=SQLAlchemy()
 
+db.init_app(app)
 
-class Products(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(100))
-
-
-
+from products import routes
+from users.routes import user
 app.register_blueprint(user,url_prefix='') 
 
 app.register_blueprint(routes.products,url_prefix='/products')
--- flask/basics/products/routes.py
+++ flask/basics/products/routes.py
@@ -1,18 +1,22 @@
 from flask import Blueprint,render_template,request
+from models import Products,db
 
 products=Blueprint('product',__name__)
 
 @products.route('/')
 def all_products():
+    items=Products.query.all()
+    print(items)
     return render_template('products.html')
 
 @products.route('/create',methods=['GET','POST'])
 def create():
     if request.method=='POST':
         title= request.form['title']
-        
-        return f"title = {title}"
-    
+        p=Products(title=title)
+        db.session.add(p)
+        db.session.commit()
+        return f"title = {title}"    
     return render_template('create.html')
 
 @products.route('/<int:id>')
