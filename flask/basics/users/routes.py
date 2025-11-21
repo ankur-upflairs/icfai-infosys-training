70e5df8ee054b464c7ab72852790c41a670d8200
ankur-upflairs <ankur.upflairs@gmail.com>
2025-11-21 13:06:48 +0530

    code

--- flask/basics/users/routes.py
+++ flask/basics/users/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint,render_template ,request
 
-user=Blueprint('user',__name__)
+user=Blueprint('user',__name__,template_folder='templates/')
 
 @user.route('/')
 def home():
@@ -9,6 +9,13 @@ def home():
 @user.route('/about/')
 def about():
     return 'this is about page'
+
+@user.route('/user/create',methods=['GET','POST'])
+def create_user():
+    if request.method == 'POST':
+        return 'form data submitted'
+    return render_template('create-user.html')
+
 
 @user.route('/users/<name>')
 def users(name):
