9a98e16c83fd3c80bc90984a3c3dafa40377b6ee
ankur-upflairs <ankur.upflairs@gmail.com>
2025-11-21 15:58:49 +0530

    code

--- flask/basics/models.py
+++ flask/basics/models.py
@@ -4,4 +4,8 @@ from app import db
 class Products(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(100))
+    price=db.Column(db.Integer,default=1)
+    imageUrl=db.Column(db.String(200),default='')
     
+    def __str__(self):
+        return f"{self.title}"
--- flask/basics/products/routes.py
+++ flask/basics/products/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,redirect,url_for
 from models import Products,db
 
 products=Blueprint('product',__name__)
@@ -6,17 +6,20 @@ products=Blueprint('product',__name__)
 @products.route('/')
 def all_products():
     items=Products.query.all()
-    print(items)
-    return render_template('products.html')
+    # print(items)
+    return render_template('products.html',items=items)
 
 @products.route('/create',methods=['GET','POST'])
 def create():
     if request.method=='POST':
         title= request.form['title']
-        p=Products(title=title)
+        price= request.form['price']
+        imageUrl= request.form['imageUrl']
+
+        p=Products(title=title,price=price,imageUrl=imageUrl)
         db.session.add(p)
         db.session.commit()
-        return f"title = {title}"    
+        return redirect(url_for('all_Products'))    
     return render_template('create.html')
 
 @products.route('/<int:id>')
